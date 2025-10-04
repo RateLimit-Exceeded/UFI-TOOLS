83c101a48815c83efa8b4c750c95626123c8e363
xyz <15380362192@163.com>
2025-10-04 16:24:11 +0800

    _

--- app/src/main/java/com/minikano/f50_sms/utils/DeviceModelChecker.kt
+++ app/src/main/java/com/minikano/f50_sms/utils/DeviceModelChecker.kt
@@ -3,10 +3,6 @@ package com.minikano.f50_sms.utils
 import android.content.Context
 import android.os.Build
 import android.util.Log
-import com.minikano.f50_sms.utils.KanoReport.Companion.reportToServer
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 object DeviceModelChecker {
     private var isUnSupportDevice = false
@@ -41,47 +37,16 @@ object DeviceModelChecker {
         }
 
         val uuid = UniqueDeviceIDManager.getUUID()
-        Log.d("kano_ZTE_LOG_devcheck", "当前设备UUID:$uuid")
-
-        try {
-            if (uuid != null) {
-                val res = KanoReport.getRemoteDeviceRegisterItem(uuid)
-                Log.d("kano_ZTE_LOG_devcheck", "线上数据返回:$res")
-                if (res != null && res.isWhiteList) {
-                    Log.d("kano_ZTE_LOG_devcheck", "线上白名单，永久放行")
-                    prefs.edit().putString("is_device_white_list", "kano").apply()
-                    return false
-                }
-                //上报信息
-                try{
-                    CoroutineScope(Dispatchers.Main).launch {
-                        reportToServer()
-                    }
-                } catch (_:Exception){}
-            }
-        } catch (e: Exception) {
-            Log.e("kano_ZTE_LOG_devcheck", "获取远程设备注册信息异常", e)
-        }
-
-        devicesBlackList.forEach {
-            Log.d("kano_ZTE_LOG_devcheck", "$it == $model ?")
-            if (it.trim().contains(model)) {
-                isUnSupportDevice = true
-            }
-        }
-        frimwareWhiteList.forEach {
-            if (firmwareVersion.contains(it.trim())) {
-                Log.d("kano_ZTE_LOG_devcheck", "检测到白名单固件，即将放行")
-                isUnSupportDevice = false
-            }
-        }
-        return isUnSupportDevice
+        Log.d("kano_ZTE_LOG_devcheck", "当前设备UUID:$uuid，已跳过远程白名单校验")
+        isUnSupportDevice = false
+        Log.d("kano_ZTE_LOG_devcheck", "已禁用本地黑名单限制，允许继续运行")
+        return false
     }
 
     fun checkIsNotUFI(context: Context):Boolean{
         val isUFI_0 = KanoUtils.isAppInstalled(context,"com.zte.web")
         val isUFI = ShellKano.runShellCommand("pm list package")
-        Log.d("kano_ZTE_LOG_devcheck", "isUFI_0：${isUFI_0},has com.zte.web? :${isUFI?.contains("com.zte.web")} ")
-        return !(isUFI != null && isUFI.contains("com.zte.web")) || !isUFI_0
+        Log.d("kano_ZTE_LOG_devcheck", "isUFI_0：${isUFI_0},has com.zte.web? :${isUFI?.contains("com.zte.web")} ，已跳过设备类型限制")
+        return false
     }
 }
